2014312e56f23552c12a1beaf3df879e0369cbb9
Tori Dykes <tori.dykes@gmail.com>
2016-11-10 17:02:21 +0100

    Updates data cleaning

--- DataCleaning.R
+++ DataCleaning.R
@@ -12,7 +12,7 @@ library(dplyr)  # for data manipulation
 ## Loading the Data ##
 
 d <- read.csv('ESS1-6e01_1_F1.csv') # ESS Data on Political Engagement
-e <- read.csv('Under 25 unemp.csv') # Eurostat Data on Unemployment
+e <- read.csv('Under 25 unemp.csv', stringsAsFactors = F) # Eurostat Data on Unemployment
 
 
 #### Cleaning Up the Data ####
@@ -36,3 +36,27 @@ eu <- subset(eu, eu$TIME == 2006 |eu$TIME == 2008 |eu$TIME == 2010 |eu$TIME == 2
 
 unique(df$cntry)
 unique(eu$GEO)
+
+## Drop EU-related observations from unemployment data ##
+
+dropnames <- c("Euro area (18 countries)", "Euro area (19 countries)", 
+               "European Union (28 countries)", "Euro area (EA11-2000, EA12-2006, EA13-2007, EA15-2008, EA16-2010, EA17-2013, EA18-2014, EA19)",
+               "European Union (25 countries)", "European Union (27 countries)")
+
+eu <- eu[! eu$GEO %in% dropnames, ]
+
+## Change name of Germany ##
+
+eu$GEO[eu$GEO=="Germany (until 1990 former territory of the FRG)"] <- "Germany"
+
+## Add country code to country names for merging ##
+
+install.packages("countrycode")
+library(countrycode)
+countrycode("Austria", "country.name", "iso2c")
+
+eu$COUNTRY <- countrycode(eu$GEO, "country.name", "iso2c")
+
+
+
+
